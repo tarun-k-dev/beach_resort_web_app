43d22eacf184750c1daeed046bda756f6cd9e9c4
Tarun <tktron007@gmail.com>
2021-08-01 20:50:37 +0530

    final

---
+++ src/Contentful.js
@@ -0,0 +1,7 @@
+const contentful = require("contentful");
+
+
+export default contentful.createClient({
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+});
